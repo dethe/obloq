d06733da62da9fe442f459910fedb4da12c143be
dethe <dethe@livingcode.org>
2014-10-18 00:30:06 -0700

    extract is working to create custom elements

--- bin/extract.js
+++ bin/extract.js
@@ -2,185 +2,106 @@
 
 var fs = require("fs");
 var path = require("path");
-var markdown = require("markdown").markdown;
+var marked = require("marked");
 var finder = require("findit")(process.cwd() + "/bloqs");
 
-var output_files = {};
-var rcount = 0;
-var wcount = 0;
+var setAside = {
+    html: [],
+    css: [],
+    js: []
+}
 
-function relative(infilename) {
-    if (infilename[0] === '/') {
-        return 'docs' + infilename.split(process.cwd() + '/bloqs')[1];
-    }
-    return infilename;
+/* Setup Markdown parsing */
+
+marked.setOptions({
+    sanitize: false
+});
+
+var renderer = new marked.Renderer();
+
+renderer.heading = function markdownHeading(text, level) {
+  var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+
+  return '<h' + level + '><a name="' +
+                escapedText +
+                 '" class="anchor" href="#' +
+                 escapedText +
+                 '"><span class="header-link"></span></a>' +
+                  text + '</h' + level + '>';
 };
 
-function outfilename(infilename, ext) {
-    var outpath;
-    var relativepath = relative(infilename);
-    var base = relativepath.split(path.extname(relativepath))[0];
-    switch (ext) {
-        case '.css':
-            outpath = 'public/css/mystyles/' + base + ext;
-            break;
-        case '.js':
-            outpath = 'public/js/mylibs/' + base + ext;
-            break;
-        case '.html':
-            outpath = base + ext;
-            break;
-        default:
-            outpath = "build/" + base + ext;
+renderer.code = function markdownCodeblock(code, language){
+    if (language === 'javascript'){
+        language = 'js';
     }
-    return outpath;
+    if (setAside[language]){
+        setAside[language].push(code);
+    }
+    return '<pre><code class="language-' + language + '">' + code + '</code></pre>';
 };
 
-function ensurepath(relpath) {
-    var parts = relpath.split("/");
-    parts.pop();
-    var buildpath = "";
-    var _results = [];
-    while (parts.length) {
-        buildpath = path.join(buildpath, parts.shift());
-        if (!path.existsSync(buildpath)) {
-            _results.push(fs.mkdirSync(buildpath, "0755"));
-        } else {
-            _results.push(void 0);
-        }
-    }
-    return _results;
-};
 
-function toHTMLTree(filename) {
-    var e, html, outpath, text, tree;
-    try {
-        rcount++;
-        text = fs.readFileSync(filename, "utf-8");
-        tree = markdown.toHTMLTree(text);
-        html = markdown.toHTML(text);
-        walk(tree, codeExcerpt);
-        outpath = outfilename(filename, ".html");
-        ensurepath(outpath);
-        return writeIfChanged(outpath, html);
-    } catch (_error) {
-        e = _error;
-        console.log("There was a problem processing file %s", filename);
-        throw e;
-    }
-};
+/* Utility Functions */
 
-function writeIfChanged(outpath, text) {
-    wcount++;
-    return fs.writeFileSync(outpath, text);
-};
+// function relative(infilename) {
+//     if (infilename[0] === '/') {
+//         return 'docs' + infilename.split(process.cwd() + '/bloqs')[1];
+//     }
+//     return infilename;
+// };
 
-function writefiles() {
-    var contents, ext, outpath, output_path, outtext;
-    var concatenated_js = [];
-    var concatenated_css = [];
-    for (output_path in output_files) {
-        outtext = output_files[output_path];
-        ext = path.extname(output_path);
-        outpath = outfilename(output_path, ext);
-        contents = output_files[output_path].join("\n\n");
-        if (ext === ".sketch") {
-            fs.writeFile(outpath, contents);
-            processor[ext.slice(1)](outpath, contents);
-        }
-    }
-    for (output_path in output_files) {
-        outtext = output_files[output_path];
-        ext = path.extname(output_path);
-        outpath = outfilename(output_path, ext);
-        contents = output_files[output_path].join("\n\n");
-        if (ext === ".html" || ext === ".css" || ext === ".js") {
-            writeIfChanged(outpath, processor[ext.slice(1)](outpath, contents));
-            if (ext === '.js') {
-                concatenated_js.push(contents);
-            }
-            if (ext === '.css') {
-                concatenated_css.push(contents);
-            }
-        }
-    }
-    writeIfChanged('public/js/script.js', concatenated_js.join('\n\n'));
-    writeIfChanged('public/css/style.css', concatenated_css.join('\n\n'));
-    return console.log('oextract read %s files and wrote %s files', rcount, wcount);
-};
+// function ensurepath(relpath) {
+//     var parts = relpath.split("/");
+//     parts.pop();
+//     var buildpath = "";
+//     var _results = [];
+//     while (parts.length) {
+//         buildpath = path.join(buildpath, parts.shift());
+//         if (!path.existsSync(buildpath)) {
+//             _results.push(fs.mkdirSync(buildpath, "0755"));
+//         } else {
+//             _results.push(void 0);
+//         }
+//     }
+//     return _results;
+// }
 
-function ttype(obj) {
-    var t;
-    t = typeof obj;
-    if (Array.isArray(obj)) {
-        t = "array";
-    }
-    return t;
-};
-
-function walk(tree, fn) {
-    var key, _results;
-    switch (ttype(tree)) {
-        case "object":
-            _results = [];
-            for (key in tree) {
-                fn(key, tree[key]);
-                _results.push(walk(tree[key], fn));
-            }
-            return _results;
-            break;
-        case "array":
-            if (tree.length === 2) {
-                fn(tree[0], tree[1]);
-                return walk(tree[1], fn);
-            } else {
-                return tree.forEach(function(subtree) {
-                    return walk(subtree, fn);
-                });
-            }
-            break;
-    }
-};
-
-function stashFragment(filename, fragment, processed) {
-    if (!processed) {
-        fragment = fragment.split("\n").slice(2).join("\n");
-    }
-    if (!output_files[filename]) {
-        output_files[filename] = [];
-    }
-    return output_files[filename].push(fragment);
-};
-
-function codeExcerpt(name, text) {
-    var base, ext, filename, outpath;
-    if (/.*code.*/.test(name)) {
-        var lines = text.trim().split("\n");
-        var parsedline = lines[0].match(/file\: +(.*)/);
-        if (!parsedline) {
-            throw new Error("no filename found for " + lines.length + " lines of code");
-        }
-        var filenames = parsedline[1];
-        var _ref = filenames.split(',');
-        var _results = [];
-        for (var _i = 0, _len = _ref.length; _i < _len; _i++) {
-            filename = _ref[_i];
-            filename = filename.trim();
-            ext = path.extname(filename);
-            base = path.basename(filename, ext);
-            outpath = base + ext;
-            _results.push(stashFragment(outpath, text));
-        }
-        return _results;
-    }
-};
 
 function extract(filename) {
     var ext = path.extname(filename);
     if (ext === ".md" || ext === ".markdown") {
-        return toHTMLTree(filename);
+        console.log('processing %s', filename);
+        var text = fs.readFileSync(filename, 'utf8');
+        var basefile = path.basename(filename, ext);
+        var dirname = path.dirname(filename);
+        var html = marked(text, {renderer: renderer});
+        console.log('writing %s', basefile + '.html');
+        fs.writeFileSync(path.join('dist/docs', basefile + '.html'), html);
+        console.log('writing %s', basefile + '.js');
+        fs.writeFileSync(path.join('dist/components', basefile + '.js'), buildComponent());
     }
-};
+}
+
+function buildComponent(){
+    var componentText = htmlToTemplate(setAside.html.join('\n')) + '\n' +
+                        cssToStylesheet(setAside.css.join('\n')) + '\n' +
+                        setAside.js.join('\n') + '\n';
+    setAside = {
+        html: [],
+        css: [],
+        js: []
+    }
+    return componentText;
+}
+
+function htmlToTemplate(html){
+    return 'var template = [\'' + html.split('\n').join('\'],\n[\'') + '\'].join(\'\');\n\n';
+}
+
+function cssToStylesheet(css){
+    return 'var cssText = [\'' + css.split('\n').join('\'],\n[\'') + '\'].join(\'\');\n' +
+        'some function to insert a new stylesheet';
+}
 
 var processor = {
     sketch: function(filename, contents) {},
@@ -197,4 +118,4 @@ var processor = {
 
 // Trigger processing
 finder.on("file", extract);
-finder.on("end", writefiles);
+// finder.on("end", writefiles);
